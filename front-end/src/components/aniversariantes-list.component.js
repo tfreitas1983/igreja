a9279ab9d31820836bc2a8fc4280aab2050662f3
Thiago Freitas <tfreitas1983@gmail.com>
2020-05-25 23:03:47 -0300

    Impressão do relatório de aniversariantes

--- front-end/src/components/aniversariantes-list.component.js
+++ front-end/src/components/aniversariantes-list.component.js
@@ -76,55 +76,60 @@ export default class AniversarioLista extends Component {
 
         const images = importAll(require.context('../images', false, /\.(png|gif|tiff|jpe?g|svg)$/))
         
+        let imprimir = null
+        if(this.state.resultado.length > 0) {
+            imprimir = <button onClick={() => window.print()} className="btn btn-info">Imprimir</button>
+        }
 
         return(
             <div>
-                <div className="birth-report">
-
+                <div className="birth-report">                    
                     <div className="row">
-                        <div className="col-md-8">
-                            <h4> Lista de aniversariantes do mês</h4>
+                        <div className="col-md-8">                                
+                            <h4> Lista de aniversariantes do mês</h4>                                
                         </div>
                     </div>
-                    
-                    <div className="row">
 
-                        <div className="col-md-4">                        
-                            <select 
-                                className="form-control" 
-                                id="mes"
-                                value={this.state.mes}
-                                onChange={this.estadoMes}>
-                                <option value="--">Selecione o mês</option>
-                                <option value="01">Janeiro</option>
-                                <option value="02">Fevereiro</option>  
-                                <option value="03">Março</option> 
-                                <option value="04"> Abril </option>
-                                <option value="05"> Maio </option>
-                                <option value="06">Junho</option>
-                                <option value="07"> Julho </option>
-                                <option value="08">Agosto</option>
-                                <option value="09">Setembro</option>
-                                <option value="10">Outubro</option>
-                                <option value="11">Novembro</option>
-                                <option value="12">Dezembro</option>                            
-                            </select>
+                    <div className="noprint">
+                        <div className="row">
+
+                            <div className="col-md-4">                        
+                                <select 
+                                    className="form-control" 
+                                    id="mes"
+                                    value={this.state.mes}
+                                    onChange={this.estadoMes}>
+                                    <option value="--">Selecione o mês</option>
+                                    <option value="01">Janeiro</option>
+                                    <option value="02">Fevereiro</option>  
+                                    <option value="03">Março</option> 
+                                    <option value="04"> Abril </option>
+                                    <option value="05"> Maio </option>
+                                    <option value="06">Junho</option>
+                                    <option value="07"> Julho </option>
+                                    <option value="08">Agosto</option>
+                                    <option value="09">Setembro</option>
+                                    <option value="10">Outubro</option>
+                                    <option value="11">Novembro</option>
+                                    <option value="12">Dezembro</option>                            
+                                </select>
+                            </div>
+
+                            <div className="col-md-3">
+                                <button 
+                                    type="submit" 
+                                    className="btn btn-success" 
+                                    onClick={this.gerarRelatorio}>
+                                Gerar
+                                </button>
+                            </div>
+
+                            <div>
+                                {imprimir}
+                            </div> 
+                            
                         </div>
-
-                        <div className="col-md-3">
-                            <button 
-                                type="submit" 
-                                className="btn btn-success" 
-                                onClick={this.gerarRelatorio}>
-                            Gerar
-                            </button>
-                        </div>
-
-                        <div>
-                            <button onClick={() => window.print()} className="btn btn-info">Imprimir</button>                       
-                        </div> 
                     </div>
-
                 </div>
 
                 <div>                    
